010171939a602523c182a0e88a25254367e779e7
Narwhal and Chee Zit <Narwhal and Chee Zit@104.194.108.61>
2017-08-28 18:46:03 -0500

    Hope this works

---
+++ src/lab0/MyNameIs.java
@@ -0,0 +1,12 @@
+package lab0;
+
+// This program prints out information about myself, Noah Dela Rosa
+
+public class MyNameIs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Noah Dela Rosa, and my birthdate is December 9, 1998.");
+	}
+
+}
---
+++ src/lab0/SquarePseudocode.java
@@ -0,0 +1,20 @@
+package lab0;
+
+/* This is a step-by-step to print out a picture of a square in the Terminal
+ * 1. Print out 7 dash marks
+ * 2. On the following line, print out a vertical line character, followed by 5 spaces, and then another vertical line
+ * 3. On the following line, repeat step 2
+ * 4. On the following line, repeat step 1
+ */
+public class SquarePseudocode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("--------");  //Before, the number of dashes was 4 horizontally
+		System.out.println("|      |");  //Changed the number of dashes to 4 in order to make the shape more square-like
+		System.out.println("|      |");
+		System.out.println("--------");
+	}
+		//The outputted picture is not completely a square, as the dimensions of the length and width are not equal
+		//Therefore, I somewhat got the desired result
+}
