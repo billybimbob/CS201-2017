83dbbbf15f981b67361e74c20dfe8c4fc7ec462a
Noah Dela Rosa <noahluvsmilkduds@gmail.com>
2017-10-06 14:10:06 -0500

    Got rid of throws Exception, and added try catch statements

--- src/lab3/GradeReader.java
+++ src/lab3/GradeReader.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 
 public class GradeReader {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		File inFile = new File("C:\\Users\\funte\\eclipse-workspace\\CS201\\src\\lab3\\grades.csv"); //the file location of grades.csv, dependent on the computer
-		Scanner input = new Scanner(inFile);
-		input.useDelimiter(","); //splits the lines into the name and the number
-		double accumulate = 0;
-		int count = 0;
-		
-		while (input.hasNextLine()) {
-			input.next(); //accounts for the name string
-			String num = input.next(); //the number string
-			accumulate += Double.parseDouble(num.substring(0, num.indexOf("%"))); //looks for only the number string and parses it
-			input.next(); //accounts for the \n
-			count ++;
+		try {
+			Scanner input = new Scanner(inFile);
+			input.useDelimiter(","); //splits the lines into the name and the number
+			double accumulate = 0;
+			int count = 0;
+			
+			while (input.hasNextLine()) {
+				input.next(); //accounts for the name string
+				String num = input.next(); //the number string
+				accumulate += Double.parseDouble(num.substring(0, num.indexOf("%"))); //looks for only the number string and parses it
+				input.next(); //accounts for the \n
+				count ++;
+			}
+			double average = accumulate/count; //determines the average
+			System.out.println("The average of all of the grades is " + average);
+			
+			input.close();
+		} catch (Exception e) {
+			System.out.println("Something went wrong");
 		}
-		double average = accumulate/count; //determines the average
-		System.out.println("The average of all of the grades is " + average);
-		
-		input.close();
 	}
 
 }
--- src/lab3/NumberFile.java
+++ src/lab3/NumberFile.java
@@ -5,31 +5,41 @@ import java.io.*;
 
 public class NumberFile {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) {
 		Scanner kboard = new Scanner(System.in);
 		String accumulate = "";
 		
 		while (true) { //is while true with a break so that the accumulator is skipped
 			System.out.print("Enter in \"Done\" when finished\nEnter in a number: ");
-			String input = kboard.nextLine(); //doesn't check if the string is a number
+			String input = kboard.nextLine();
 			if (input.equals("Done") || input.equals("done"))
 				break; // breaks out of the loop when "done" is entered
 					   // accounts for "done" being lowercase
-			accumulate = accumulate + input + " "; //accumulator for all the numbers
+			try {
+				Integer.parseInt(input);
+				accumulate = accumulate + input + " "; //accumulator for all the numbers
+			} catch (Exception e) {
+				System.out.println("That is not a number, please try again");
+			}
 			System.out.println("");
 		}
+		
 		System.out.print("\nEnter a name for the numbers to be\nWritten to a file: ");
 		String name = kboard.nextLine() + ".txt"; //makes the file name a text file
 		System.out.println("");
-		FileWriter outFile = new FileWriter("C:\\Users\\funte\\eclipse-workspace\\CS201\\src\\lab3\\" + name); //sets the file location
-		//file location specific to my computer, may have to change
-		
-		BufferedWriter output = new BufferedWriter(outFile);
-        output.write(accumulate); //writes accumulated string to file
-        
-        output.close();
+		try {
+			FileWriter outFile = new FileWriter("C:\\Users\\funte\\eclipse-workspace\\CS201\\src\\lab3\\" + name); //sets the file location
+			//file location specific to my computer, may have to change
+			
+			BufferedWriter output = new BufferedWriter(outFile);
+	        output.write(accumulate); //writes accumulated string to file
+	        
+	        output.close();
+	        System.out.println("Your numbers have been written to a text file");
+		} catch (Exception e) {
+			System.out.println("Something went wrong"); //not really sure when this would happen, program ends, and file isn't written
+		}
 		kboard.close();
-		System.out.println("Your numbers have been written to a text file");
 	}
 
 }
