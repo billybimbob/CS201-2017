1678b0b5ce026236b08cbec3968977e9d915e60f
Noah Dela Rosa <noahluvsmilkduds@gmail.com>
2017-11-19 23:41:12 -0600

    Added more comments

--- src/lab7/InsertionSort.java
+++ src/lab7/InsertionSort.java
@@ -4,14 +4,14 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 
-		String[] nameList = {"cat", "fat", "python", "ruby", "scala"};
+		String[] nameList = {"cat", "bat", "fat", "dog", "apple", "egg"};
 		
 		for (int i = 0; i < nameList.length-1; i++) {
 			int j = i+1; //find where to insert this value in sorted section  
-			while(j >= 1 && nameList[j].compareToIgnoreCase(nameList[j-1]) > 0) {
-				String store = nameList[j]; //if not sorted, swap
-				nameList[j] = nameList[j+1];
-				nameList[j+1] = store;
+			while(j >= 1 && nameList[j].compareToIgnoreCase(nameList[j-1]) < 0) { //stays in loop until at beginning of list or moving word is no longer alphabetically backwards
+				String store = nameList[j]; //if not sorted, swap				  //first conditional establishes sorted section
+				nameList[j] = nameList[j-1];
+				nameList[j-1] = store;
 				j-=1;
 			} //breaks out of loop when at end of list or when sorted section encountered
 			//breaking inserts j value essentially
@@ -20,6 +20,7 @@ public class InsertionSort {
 		for (String name: nameList) { //prints list
 			System.out.println(name);
 		}
+		//System.out.println("");
 	}
 
 }
